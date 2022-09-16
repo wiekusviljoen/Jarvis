039c7fdf967655a2359753323b29675c26b79f68
wiekusviljoen <wiekusviljoen@gmail.com>
2022-09-16 18:16:30 +0200

    commit on master

--- app.js
+++ app.js
@@ -2,6 +2,54 @@ const startBtn = document.querySelector("#start");
 const stopBtn = document.querySelector("#stop");
 const speakBtn = document.querySelector("#speak");
 
+//weather
+
+function weather(location) {
+  const weatherCont = document.querySelector(".temp").querySelectorAll("*");
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6860c1da83e8c5136cf4e8c6dd709741`;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let data = JSON.parse(this.responseText);
+      weatherCont[0].textContent = `Location :${data.name} `;
+      weatherCont[1].textContent = `Country :${data.sys.country} `;
+      weatherCont[2].textContent = `Weather type :${data.weather[0].main} `;
+      weatherCont[3].textContent = `Weather description :${data.weather[0].description} `;
+      weatherCont[4].src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png `;
+      weatherCont[5].textContent = `Original Temperature :${
+        ktc[data.main.temp]
+      } `;
+      weatherCont[6].textContent = `feels like :${ktc[data.main.feels_like]} `;
+      weatherCont[7].textContent = `Min temperature :${
+        ktc[data.main.temp_min]
+      } `;
+      weatherCont[8].textContent = `Max temperature :${
+        ktc[data.main.temp_max]
+      } `;
+      weatherStatement = `sir the weather in ${data.name}is ${
+        data.weather[0].description
+      } and the temperature feels like ${ktc(data.main.feels_like)}`;
+    } else {
+      weatherCont[0].textContent = "Weather Info Not Found";
+    }
+  };
+  xhr.send();
+}
+
+//convert to celsius
+
+function ktc(k) {
+  k = k - 273.15;
+  return k.toFixed(2);
+}
+
+//calling weather
+
+weather("visakhapatnam");
+
 //speech recoginition setup
 
 const SpeechRecognition =
